2e631afc592f928b30bbc08f3d0c6e45fab0a6cb
Luis Picurelli <luis@yeeply.com>
2014-04-30 01:25:37 +0200

    Creada aplicacion de prueba con status

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PruebaApp::Application.config.secret_token = '903532f13b10a405ca5fd2e91b34f9e900b0121e6a49ff6bfb3b25cd86cfc40b3874a2ee575efbd292fb51602a5ce3b588e3add171dee6fc0fe5cd807e80eadf'
